8e8c4f9f2545edd07ef2fe35a52da2f3c59512b8
Anna Smolentzov <annasmolentzov@gmail.com>
2019-03-19 13:59:07 -0600

    Creates initial migration with tables for favorites, playlists, playlist_favorites

---
+++ db/migrations/20190319135122_initial.js
@@ -0,0 +1,37 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('favorites', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('artist_name');
+      table.string('genre');
+      table.integer('rating');
+      
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('playlists', function(table) {
+      table.increments('id').primary();
+      table.string('playlist_name');
+      
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('playlist_favorites', function(table) {
+      table.increments('id').primary();
+      table.integer('favorite_id').unsigned();
+      table.foreign('favorite_id').references('favorites.id');
+      table.integer('playlist_id').unsigned();
+      table.foreign('playlist_id').references('playlists.id');
+      
+      table.timestamps(true, true);
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('playlist_favorites'),
+    knex.schema.dropTable('playlists'),
+    knex.schema.dropTable('favorites'),
+  ])
+};
