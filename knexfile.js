321656d861945f9a3970a21cec24eaa958d9223f
Lance Taylor <lance.taylor98@hotmail.com>
2019-03-20 16:37:35 -0600

    Adds seed data for application. Seeds key for dev and test env

--- knexfile.js
+++ knexfile.js
@@ -8,14 +8,20 @@ module.exports = {
     migrations: {
       directory: './db/migrations'
     },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
     useNullAsDefault: true
   },
-  
+
   test: {
     client: 'pg',
     connection: 'postgres://localhost/play_app_test',
     migrations: {
       directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   },
