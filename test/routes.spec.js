f8e8050bc490f24d802fce319ce1a6a3b01556b4
Anna Smolentzov <annasmolentzov@gmail.com>
2019-03-20 19:16:39 -0600

    Adds test for GET api/v1/favorites

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -13,5 +13,21 @@ describe('Client Routes', () => {
 })
 
 describe('API Routes', () => {
-
-})
+  describe('GET /api/v1/favorites', () => {
+    it('should return all of the favorites', done => {
+      chai.request(server)
+        .get('/api/v1/favorites')
+        .end((error, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(1);
+          response.body[0].should.have.property('name');
+          response.body[0].should.have.property('artist_name');
+          response.body[0].should.have.property('genre');
+          response.body[0].should.have.property('rating');
+          done();
+        });
+    });
+  });
+});
