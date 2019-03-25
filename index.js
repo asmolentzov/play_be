67245375461802117c50d8b81dc214d4b1084040
Anna Smolentzov <annasmolentzov@gmail.com>
2019-03-25 11:51:23 -0600

    Refactors favorites get all and post to MVC per class

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+const favorites = require('./lib/routes/api/v1/favorites');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
@@ -25,37 +27,7 @@ app.use((req, res, next) => {
   }
 });
 
-app.get('/api/v1/favorites', (request, response) => {
-  database('favorites').select()
-    .then(favorites => {
-      response.status(200).json(favorites);
-    })
-    .catch(error => {
-      response.status(500).json({ error })
-    });
-});
-
-app.post('/api/v1/favorites', (request, response) => {
-  const favorite = request.body.favorites;
-  for(let requiredParameter of ['id', 'name', 'artist_name', 'genre', 'rating']) {
-    if(!favorite[requiredParameter]) {
-      return response
-        .status(400)
-        .send({ error: `You're missing a "${requiredParameter}" property.` });
-    } else if(isNaN(favorite['rating']) || favorite['rating'] > 100 || favorite['rating'] < 0) {
-      return response
-        .status(400)
-        .send({ error: 'Rating must be a number between 0 - 100.' })
-    }
-  }
-  database('favorites').insert(favorite, ['id', 'name', 'artist_name', 'genre', 'rating'])
-    .then(favorite => {
-      response.status(201).json(favorite)
-    })
-    .catch(error => {
-      response.status(500).json({ error })
-    });
-});
+app.use('/api/v1/favorites', favorites);
 
 app.get('/api/v1/favorites/:id', (request, response) => {
   database('favorites').where('id', request.params.id).select()
