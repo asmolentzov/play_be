b0bff19ed892332139a41a00fa775fe02a499666
Lance Taylor <lance.taylor98@hotmail.com>
2019-03-26 06:41:57 -0600

    Adds test to make sure it returns a 500 is a parameter is incorrect.

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -283,8 +283,25 @@ describe('API Routes', () => {
       chai.request(server)
       .post('/api/v1/playlists/1/favorites/2')
         .end((error, response) => {
-          // eval(pry.it)
           response.should.have.status(201);
+          done();
+      });
+    })
+
+    it('should error out if uri request params are missing or do not exist, favorite_id', done => {
+      chai.request(server)
+      .post('/api/v1/playlists/1/favorites/taco')
+        .end((error, response) => {
+          response.should.have.status(500);
+          done();
+      });
+    })
+
+    it('should error out if uri request params are missing or do not exist, playlist_id', done => {
+      chai.request(server)
+      .post('/api/v1/playlists/taco/favorites/1')
+        .end((error, response) => {
+          response.should.have.status(500);
           done();
       });
     })
